c74b2621ce06815734ce27b8831d98c4bee1f2de
lhethert <lhethert@fastmail.fm>
2019-12-09 17:34:34 +1100

    Added charts to the skills section of the site using d3.js - one chart for tools and one chart for languages and frameworks.

---
+++ assets/charts.js
@@ -0,0 +1,118 @@
+charts = {};
+(function(){
+  if (!charts) {
+    charts = {};
+  }
+
+  //set up svg using margin conventions - we'll need plenty of room on the left for labels
+  var margin = {
+      top: 0,
+      right: 100,
+      bottom: 0,
+      left: 100
+  };
+
+  var width = 420 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+  // function to create charts
+  charts.create = function(data, parentSelector, colorMin, colorMax) {
+      var svg = parentSelector.append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var x = d3.scale.linear()
+          .range([0, width])
+          .domain([0, d3.max(data, function (d) {
+              return d.value;
+          })]);
+
+      var y = d3.scale.ordinal()
+          .rangeRoundBands([height, 0], .1)
+          .domain(data.map(function (d) {
+              return d.name;
+          }));
+
+      //make y axis to show bar names
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          //no tick marks
+          .tickSize(0)
+          .orient("left");
+
+      var gy = svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+
+      var colorScale = d3.scale.linear().domain([0, 5]).range([colorMin, colorMax]);
+      var bars = svg.selectAll(".bar")
+          .data(data)
+          .enter()
+          .append("g")
+          .style("fill", function(d, i) { return colorScale(d.value); })
+
+      //append rects
+      bars.append("rect")
+          .attr("class", "bar")
+          .attr("y", function (d) {
+              return y(d.name);
+          })
+          .attr("height", y.rangeBand())
+          .attr("x", 0)
+          .attr("width", function (d) {
+              //return x(d.value);
+            return 0; // initially zero
+          });
+
+      bars.selectAll("rect")
+          .transition()
+          .duration(1000)
+          .ease('bounce')
+          .attr("width", function(d) { return x(d.value); })
+          .delay(function(d,i) { return(i*100); } );
+
+      //retriev the proficiency string based on the stored value
+      var getProficiencyStr = function(value) {
+          var strOut = "";
+          switch(value) {
+              case 5:
+                  strOut = "Expert";
+                  break;
+              case 4:
+                  strOut = "Advanced";
+                  break;
+              case 3:
+                  strOut = "Intermediate";
+                  break;
+              case 2:
+                  strOut = "Novice";
+                  break;
+              case 1:
+                  strOut = "Basic knowledge";
+                  break;
+              default:
+                  strOut = "Invalid enum";
+          }
+          return strOut;
+      };
+
+      //add a value label to the right of each bar
+      bars.append("text")
+          .attr("class", "label")
+          //y position of the label is halfway down the bar
+          .attr("y", function (d) {
+              return y(d.name) + y.rangeBand() / 2 + 4;
+          })
+          //x position is 3 pixels to the right of the bar
+          .attr("x", function (d) {
+              return x(d.value) + 3;
+          })
+          .text(function (d) {
+              return getProficiencyStr( Number(d.value) );
+          });
+    }
+})();
+
+
